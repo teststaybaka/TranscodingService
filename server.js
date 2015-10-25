05a32e9d401cdedcb6e8cb19838f06e79ae0fcfa
teststaybaka <teststaybaka@gmail.com>
2015-10-25 03:31:25 +0000

    Child process to run ffmpeg

--- server.js
+++ server.js
@@ -1,13 +1,18 @@
 var spawn = require('child_process').spawn;
 
-var worker = spawn('ffmpeg', []);
+var width = 640;
+var height = 480;
+//var worker = spawn('ls', ['-lh']);
+//var worker = spawn('ffmpeg', ['-i', '1.mp4', 'test.mp4']);
+var args = ['-i', '1.mp4', '-y', '-c:a', 'libfdk_aac', '-b:a', '320k', '-filter:v', 'scale=iw*min('+width+'/iw\\, '+height+'/ih):ih*min('+width+'/iw\\, '+height+'/ih), pad='+width+':'+height+':('+width+'-iw*min('+width+'/iw\\, '+height+'/ih))/2:('+height+'-ih*min('+width+'/iw\\, '+height+'/ih))/2', '-c:v', 'libx264', '-b:v', '900k', '-maxrate', '1200k', '-bufsize', '2000k', '-profile:v', 'main', '-level', '4.0', '-pix_fmt', 'yuv420p', '-movflags', 'faststart','-r:v', '24', 'test.mp4'];
+console.log(args);
+var worker = spawn('ffmpeg', args);
 worker.stdout.on('data', function (data) {
-  data = data.toString();
-  console.log(data);
-}
-worker.on('data', function (data) {
-  console.log('transcoding error:'+data.toString());
+    console.log(data.toString());
+});
+worker.stderr.on('data', function (data) {
+    console.log(data.toString());
 });
 worker.on('close', function (code) {
-  console.log('worker exited with code:'+code);
+    console.log('worker exited with code:'+code);
 });
