98e7402d82f6c9bab90e47b9e16b3548b2e074a8
teststaybaka <teststaybaka@gmail.com>
2015-11-15 18:26:58 +0000

    fix typo

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ var height = 480;
 var args = ['-i', '1.mp4', '-y', '-c:a', 'libfdk_aac', '-b:a', '320k', '-filter:v', 'scale=iw*min('+width+'/iw\\, '+height+'/ih):ih*min('+width+'/iw\\, '+height+'/ih), pad='+width+':'+height+':('+width+'-iw*min('+width+'/iw\\, '+height+'/ih))/2:('+height+'-ih*min('+width+'/iw\\, '+height+'/ih))/2', '-c:v', 'libx264', '-b:v', '800k', '-maxrate', '1200k', '-bufsize', '2000k', '-profile:v', 'main', '-level', '4.0', '-pix_fmt', 'yuv420p', '-r:v', '24', '-movflags', 'faststart', 'test.mp4'];
 console.log(args);
 var worker = spawn('ffmpeg', args);
-worker.stdout.on('data', fonction (data) {
+worker.stdout.on('data', function (data) {
     console.log(data.toString());
 });
 worker.stderr.on('data', function (data) {
